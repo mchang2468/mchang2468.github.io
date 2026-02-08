2c9f80d1c2a4bd769fa47f1e09debe218d28ad9c
mchang2468 <menchang.06@gmail.com>
2026-02-08 15:00:03 -0500

    Fix base path for production

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Change to just '/' for user sites
+  base: process.env.NODE_ENV === 'production' ? '/mc.github.io/' : '/',
 })
